7589750a8b7fca6fe9502b9175c5e4653df5045b
steliq <steliq@github.com>
2019-07-13 11:13:44 +0300

    Am incarcat proiectul Stelica

---
+++ Stelica/src/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("SAlut, ce mai faci?");
+		System.out.println("Ma bucur ca ai revenit si ca putem lucra impreuna");
+
+	}
+
+}
